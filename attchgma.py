0ee8779341dd2d85fd4dc47cfb2fa218ed755105
RahulHN <47886062+RahulHN@users.noreply.github.com>
2019-03-22 15:32:45 +0530

    Add files via upload

---
+++ attchgma.py
@@ -0,0 +1,66 @@
+#Sending mail with attachments 
+# from your Gmail account  
+  
+# libraries to be imported 
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+   
+fromaddr = "rahulhn2000@gmail.com"
+toaddr = "rahulhn999@gmail.com"
+   
+# instance of MIMEMultipart 
+msg = MIMEMultipart() 
+  
+# storing the senders email address   
+msg['From'] = fromaddr 
+  
+# storing the receivers email address  
+msg['To'] = toaddr 
+  
+# storing the subject  
+msg['Subject'] = "ABC"
+  
+# string to store the body of the mail 
+body = "XYZ"
+  
+# attach the body with the msg instance 
+msg.attach(MIMEText(body, 'plain')) 
+  
+# open the file to be sent  
+filename = "r31.py"
+attachment = open("/home/bl210/r31.py", "rb") 
+  
+# instance of MIMEBase and named as p 
+p = MIMEBase('application', 'octet-stream') 
+  
+# To change the payload into encoded form 
+p.set_payload((attachment).read()) 
+  
+# encode into base64 
+encoders.encode_base64(p) 
+   
+p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+  
+# attach the instance 'p' to instance 'msg' 
+msg.attach(p) 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login(fromaddr, "9482288197") 
+  
+# Converts the Multipart msg into a string 
+text = msg.as_string() 
+  
+# sending the mail 
+s.sendmail(fromaddr, toaddr, text) 
+  
+# terminating the session 
+s.quit()
